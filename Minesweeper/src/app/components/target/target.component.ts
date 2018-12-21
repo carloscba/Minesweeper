eff254d7e9acdd9cca652f679739c16a746d6b4d
M8-Desktop <carloscba1@gmail.com>
2018-12-21 13:20:31 -0300

    Buscando targets vecinos

--- Minesweeper/src/app/components/target/target.component.ts
+++ Minesweeper/src/app/components/target/target.component.ts
@@ -9,20 +9,21 @@ export class TargetComponent implements OnInit {
 
   @Input() targets:any[];
   @Input() position:any[];
+  
   public isTarget:boolean
-
+  public neighborTargets:number = 0;
   constructor() { 
     
   }
 
   ngOnInit() {
-    this.isTarget = this.checkIsTarget()
+    this.isTarget = this.checkIsTarget(this.position)
   }
 
-  checkIsTarget():boolean{
+  checkIsTarget(position):boolean{
     let targetFlag = false
     this.targets.forEach((element)=>{
-      if(element[0] === this.position[0] && element[1] === this.position[1]){
+      if(element[0] === position[0] && element[1] === position[1]){
         targetFlag = true;
         return true;
       }
@@ -32,10 +33,32 @@ export class TargetComponent implements OnInit {
 
   selected():void{
     (this.isTarget) && alert('LOSE')
+    this.getNeighbors()
   }
   
-  countNeighbors(){
+  getNeighbors():void{
+    let neighbors = [
+      [this.position[0]-1, this.position[1]-1],
+      [this.position[0]-1, this.position[1]],
+      [this.position[0]-1, this.position[1]+1],
 
+      [this.position[0], this.position[1]-1],
+      [this.position[0], this.position[1]+1],  
+      
+      [this.position[0]+1, this.position[1]-1],
+      [this.position[0]+1, this.position[1]],
+      [this.position[0]+1, this.position[1]+1],      
+    ];
+
+    this.countNeighborsTargets(neighbors);
+  }
+
+  countNeighborsTargets(neighbors):void{
+    let count = 0
+    neighbors.forEach((neighbor)=>{
+      count = (this.checkIsTarget(neighbor)) ? count + 1 : count 
+    })
+    this.neighborTargets = count;
   }
 
 }
